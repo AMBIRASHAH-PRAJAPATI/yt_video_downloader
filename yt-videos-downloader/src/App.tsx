7b1fc668ca0a66bc6f61d039fe307daa851a5efc
Ambirashah <ambirashah@codesmithdev.com>
2025-08-05 17:16:07 +0530

    removed unsed

--- yt-videos-downloader/src/App.tsx
+++ yt-videos-downloader/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Home from "./web/Home";
 
 function App() {
--- yt-videos-downloader/src/extension/content.tsx
+++ yt-videos-downloader/src/extension/content.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { DownloadButton } from "../components/DownloadButton";
 import { DownloadPopover } from "../components/DownloadPopover";
